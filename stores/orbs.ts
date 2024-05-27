c533a0108dba9c595656b479a259ce025321a405
Danny Hofmann <dhofmann@mobivention.com>
2024-05-27 16:39:25 +0200

    Trying to implement two different speeds

--- stores/orbs.ts
+++ stores/orbs.ts
@@ -1,52 +1,55 @@
-import { defineStore } from 'pinia';
-import { ref } from 'vue';
+import { defineStore } from 'pinia'
+import { ref } from 'vue'
 
 interface Resource {
-    id: number;
-    name: string;
-    count: number;
-    icon: string;
+  id: number
+  name: string
+  count: number
+  icon: string
 }
 
 export const useOrbsStore = defineStore('orbs', () => {
-    const dust = ref<Resource[]>([]);
-    const orbs = ref<Resource[]>([]);
+  const dust = ref<Resource[]>([])
+  const orbs = ref<Resource[]>([])
 
-    const initData = () => {
-        dust.value = [
-            { id: 1, name: 'Black', count: 0, icon: 'https://picsum.photos/200/300' },
-            { id: 2, name: 'Blue', count: 0, icon: 'https://picsum.photos/200/300' },
-            { id: 3, name: 'Green', count: 0, icon: 'https://picsum.photos/200/300' },
-            { id: 4, name: 'Orange', count: 0, icon: 'https://picsum.photos/200/300' },
-            { id: 5, name: 'Purple', count: 0, icon: 'https://picsum.photos/200/300' },
-        ];
+  // New states
+  const amountOfCharges = ref(0)
+  const chargeSpeed = ref(10000) // 10 seconds for auto charge
+  const manualChargeSpeed = ref(chargeSpeed.value) // Manual speed starts the same as auto speed
 
-        orbs.value = [
-            { id: 1, name: 'Black', count: 0, icon: 'https://picsum.photos/200/300' },
-            { id: 2, name: 'Blue', count: 0, icon: 'https://picsum.photos/200/300' },
-            { id: 3, name: 'Green', count: 0, icon: 'https://picsum.photos/200/300' },
-            { id: 4, name: 'Orange', count: 0, icon: 'https://picsum.photos/200/300' },
-            { id: 5, name: 'Purple', count: 0, icon: 'https://picsum.photos/200/300' },
-        ];
-    };
+  const initData = () => {
+    dust.value = [
+      { id: 1, name: 'Black', count: 0, icon: 'https://picsum.photos/200/300' },
+      { id: 2, name: 'Blue', count: 0, icon: 'https://picsum.photos/200/300' },
+      { id: 3, name: 'Green', count: 0, icon: 'https://picsum.photos/200/300' },
+      { id: 4, name: 'Orange', count: 0, icon: 'https://picsum.photos/200/300' },
+      { id: 5, name: 'Purple', count: 0, icon: 'https://picsum.photos/200/300' },
+    ]
 
-    const getResource = (resourceId: number, resourceList: Resource[]) => {
-        return resourceList.find(r => r.id === resourceId);
-    };
+    orbs.value = [
+      { id: 1, name: 'Black', count: 0, icon: 'https://picsum.photos/200/300' },
+      { id: 2, name: 'Blue', count: 0, icon: 'https://picsum.photos/200/300' },
+      { id: 3, name: 'Green', count: 0, icon: 'https://picsum.photos/200/300' },
+      { id: 4, name: 'Orange', count: 0, icon: 'https://picsum.photos/200/300' },
+      { id: 5, name: 'Purple', count: 0, icon: 'https://picsum.photos/200/300' },
+    ]
+  }
 
-    const incrementResource = (resourceId: number, resourceList: Resource[]) => {
-        const resource = getResource(resourceId, resourceList);
-        if (resource) {
-            resource.count++;
-        }
-    };
+  const getResource = (resourceId: number, resourceList: Resource[]) => {
+    return resourceList.find(r => r.id === resourceId)
+  }
 
-    const decrementResource = (resourceId: number, resourceList: Resource[]) => {
-        const resource = getResource(resourceId, resourceList);
-        if (resource && resource.count > 0) {
-            resource.count--;
-        }
-    };
+  const incrementResource = (resourceId: number, resourceList: Resource[]) => {
+    const resource = getResource(resourceId, resourceList)
+    if (resource)
+      resource.count++
+  }
 
-    return { dust, orbs, getResource, incrementResource, decrementResource, initData };
-});
+  const decrementResource = (resourceId: number, resourceList: Resource[]) => {
+    const resource = getResource(resourceId, resourceList)
+    if (resource && resource.count > 0)
+      resource.count--
+  }
+
+  return { dust, orbs, amountOfCharges, chargeSpeed, manualChargeSpeed, getResource, incrementResource, decrementResource, initData }
+})
