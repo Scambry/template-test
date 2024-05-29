de23f94980ac8acb44e970a6a2eb508562986485
Danny Hofmann <danny.hofmann94@gmail.com>
2024-05-29 03:52:30 +0200

    Top navbar timer

--- components/app/AppTopbar.vue
+++ components/app/AppTopbar.vue
@@ -1,10 +1,15 @@
 <script setup lang='ts'>
 import { computed, onBeforeUnmount, onMounted, ref } from 'vue'
+import { useOrbsStore } from '~/stores/orbs' // Adjust the path according to your project structure
+import { useLayout } from '~/composables/layout'
+import { useThemeStore } from '~/stores/theme' // Adjust the path according to your project structure
 
 defineEmits(['menuToggle'])
 const { onMenuToggle } = useLayout()
 const outsideClickListener = ref(null)
 const topbarMenuActive = ref(false)
+
+const orbsStore = useOrbsStore()
 
 onMounted(() => {
   bindOutsideClickListener()
@@ -36,7 +41,7 @@ function bindOutsideClickListener() {
 
 function unbindOutsideClickListener() {
   if (outsideClickListener.value) {
-    document.removeEventListener('click', outsideClickListener)
+    document.removeEventListener('click', outsideClickListener.value)
     outsideClickListener.value = null
   }
 }
@@ -56,8 +61,6 @@ const op = ref<any>(null)
 function toggle(event: any) {
   op.value.toggle(event)
 }
-
-
 </script>
 
 <template>
@@ -69,59 +72,72 @@ function toggle(event: any) {
     <button class="p-link layout-menu-button layout-topbar-button" @click="onMenuToggle()">
       <i class="pi pi-bars" />
     </button>
+    <div class="layout-topbar-center">
+      <!-- Display fillCounter -->
+      <div class="fill-counter">
+        {{ orbsStore.fillCounter }}
+      </div>
+      <!-- Mini Progress Bar -->
+      <div class="mini-progress-bar">
+        <div
+            class="mini-progress-bar-fill"
+            :style="{ width: orbsStore.progress + '%' }"
+        ></div>
+      </div>
+    </div>
+
 
     <button class="p-link layout-topbar-menu-button layout-topbar-button" @click="onTopBarMenuButton()">
       <i class="pi pi-ellipsis-v" />
     </button>
 
     <div class="layout-topbar-menu" :class="topbarMenuClasses">
-
-
       <button class="p-link layout-topbar-button" @click="toggle">
         <i class="pi pi-cog" />
         <span>Settings</span>
       </button>
     </div>
+
     <client-only>
       <OverlayPanel id="overlay_panel" ref="op" append-to="body" style="width: 200px">
         <h6>Theme</h6>
         <div class="field-radiobutton">
           <RadioButton
-            v-model="themeStore.themeName"
-            input-id="aura-dark"
-            name="layoutColorMode"
-            value="aura-dark"
-            @change="themeStore.setTheme('aura-dark')"
+              v-model="themeStore.themeName"
+              input-id="aura-dark"
+              name="layoutColorMode"
+              value="aura-dark"
+              @change="themeStore.setTheme('aura-dark')"
           />
           <label for="aura-dark">Aura Dark</label>
         </div>
         <div class="field-radiobutton">
           <RadioButton
-            v-model="themeStore.themeName"
-            input-id="aura-light"
-            name="layoutColorMode"
-            value="aura-light"
-            @change="themeStore.setTheme('aura-light')"
+              v-model="themeStore.themeName"
+              input-id="aura-light"
+              name="layoutColorMode"
+              value="aura-light"
+              @change="themeStore.setTheme('aura-light')"
           />
           <label for="aura-light">Aura Light</label>
         </div>
         <div class="field-radiobutton">
           <RadioButton
-            v-model="themeStore.themeName"
-            input-id="lara-dark"
-            name="layoutColorMode"
-            value="lara-dark"
-            @change="themeStore.setTheme('lara-dark')"
+              v-model="themeStore.themeName"
+              input-id="lara-dark"
+              name="layoutColorMode"
+              value="lara-dark"
+              @change="themeStore.setTheme('lara-dark')"
           />
           <label for="lara-dark">Lara Dark</label>
         </div>
         <div class="field-radiobutton">
           <RadioButton
-            v-model="themeStore.themeName"
-            input-id="lara-light"
-            name="layoutColorMode"
-            value="lara-light"
-            @change="themeStore.setTheme('lara-light')"
+              v-model="themeStore.themeName"
+              input-id="lara-light"
+              name="layoutColorMode"
+              value="lara-light"
+              @change="themeStore.setTheme('lara-light')"
           />
           <label for="lara-light">Lara Light</label>
         </div>
@@ -129,40 +145,32 @@ function toggle(event: any) {
         <h6>Primary Color</h6>
         <div class="flex">
           <div
-            style="width:2rem;height:2rem;border-radius:6px; "
-            class="bg-green-500  mr-3  cursor-pointer"
-            @click="themeStore.setColor('green')"
+              style="width:2rem;height:2rem;border-radius:6px; "
+              class="bg-green-500  mr-3  cursor-pointer"
+              @click="themeStore.setColor('green')"
           />
           <div
-            style="width:2rem;height:2rem;border-radius:6px"
-            class="bg-blue-500 mr-3 cursor-pointer"
-            @click="themeStore.setColor('blue')"
+              style="width:2rem;height:2rem;border-radius:6px"
+              class="bg-blue-500 mr-3 cursor-pointer"
+              @click="themeStore.setColor('blue')"
           />
           <div
-            style="width:2rem;height:2rem;border-radius:6px;"
-            class="bg-teal-500 mr-3 cursor-pointer"
-            @click="themeStore.setColor('teal')"
+              style="width:2rem;height:2rem;border-radius:6px;"
+              class="bg-teal-500 mr-3 cursor-pointer"
+              @click="themeStore.setColor('teal')"
           />
           <div
-            style="width:2rem;height:2rem;border-radius:6px"
-            class="bg-purple-500 mr-3 cursor-pointer"
-            @click="themeStore.setColor('purple')"
+              style="width:2rem;height:2rem;border-radius:6px"
+              class="bg-purple-500 mr-3 cursor-pointer"
+              @click="themeStore.setColor('purple')"
           />
           <div
-            style="width:2rem;height:2rem;border-radius:6px"
-            class="bg-amber-500 mr-3 cursor-pointer"
-            @click="themeStore.setColor('amber')"
+              style="width:2rem;height:2rem;border-radius:6px"
+              class="bg-amber-500 mr-3 cursor-pointer"
+              @click="themeStore.setColor('amber')"
           />
         </div>
       </OverlayPanel>
     </client-only>
   </div>
 </template>
-
-<style lang="scss" scoped>
-.layout-topbar-menu {
-  .p-link {
-      color: var(--primary-color);
-  }
-}
-</style>
